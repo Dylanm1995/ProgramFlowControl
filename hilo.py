d500d5ff981524c72ba84a1a76fe536739894b62
Dylanm1995 <dylanmartin555@gmail.com>
2020-06-01 12:01:59 +0100

    pass statement and Hi-Lo game

---
+++ hilo.py
@@ -0,0 +1,25 @@
+low = 1
+high = 1000
+
+print("Please think of a number between {} and {}".format(low, high))
+input("Press ENTER to start")
+
+guesses = 1
+while True:
+    guess = low + (high - low) // 2
+    high_low = input("My guess is {}. Should I guess higher or lower? "
+                     "Enter h or l, or c if my guess was correct "
+                     .format(guess)).casefold()
+
+    if high_low == "h":
+        # Guess higher. The low end of the range becomes 1 greater than the guess.
+        low = guess + 1
+    elif high_low == "l":
+        # Guess lower. the high end of the ranger becomes 1 lower than the guess.
+        high = guess - 1
+    elif high_low == "c":
+        print("I got it in {} guesses!".format(guesses))
+    else:
+        print("Please enter h, l or c")
+
+guesses = guesses + 1
