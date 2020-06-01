552ae80fde050a34e9d839cc8f948cf22c6bfca3
Dylanm1995 <dylanmartin555@gmail.com>
2020-06-01 10:37:18 +0100

    the random module and import

--- guessinggame.py
+++ guessinggame.py
@@ -1,30 +1,24 @@
-answer = 5
+import random
 
+highest = 10
+answer = random.randint(1, highest)
+guess = 0   # initialize to any number that doesnt equal the answer
 print("Please guess number between 1 and 10: ")
-guess = int(input())
 
-if guess == answer:
-    print("you got it first time")
-else:
-    if guess < answer:
-        print("Please guess higher")
-    else:   # guess must be greater than answer
-        print("Please guess lower")
+while guess!= answer:
     guess = int(input())
+
+    if guess == 0:
+        break
     if guess == answer:
         print("Well done, you guessed it")
+        break
     else:
-        print("Sorry, you have not guessed correctly")
+        if guess < answer:
+            print("Please guess higher")
+        else:   # guess must be greater than answer
+            print("Please guess lower")
 
 
-# if guess < answer:
-#     print("Please guess higher")
-#     guess = int(input())
-#     if guess == answer:
-#         print("Well done, you guessed it")
-#     else:
-#         print("Sorry, you have not guessed correctly")
-# elif guess > answer:
-#     print("Please guess lower")
-# else:
-#     print("You got it first time")
+
+
